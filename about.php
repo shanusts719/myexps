abdf5874b1370ee384d53970d0274697cf784977
shanusts719 <shanibashanif@gmail.com>
2021-01-19 10:15:54 +0530

    testing first commit add about us file

---
+++ about.php
@@ -0,0 +1,261 @@
+<?php include('session.php'); 
+$successmsg = "";
+$errormsg = "";
+$msg1 = "";
+$msg2 = "";
+$counter = "0";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+	<!-- Start of HubSpot Embed Code -->
+	  <script type="text/javascript" id="hs-script-loader" async defer src="//js.hs-scripts.com/8590486.js"></script>
+	  <!-- End of HubSpot Embed Code -->
+
+	  <!-- Global site tag (gtag.js) - Google Analytics -->
+	  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-179802585-1"></script>
+	  <script>
+	    window.dataLayer = window.dataLayer || [];
+	    function gtag(){dataLayer.push(arguments);}
+	    gtag('js', new Date());
+
+	    gtag('config', 'UA-179802585-1');
+	  </script>
+	  <!-- End of Google Analytics Code -->
+	  	<meta name="p:domain_verify" content="a50c307b0c7404b0749b9bda361fe5f5"/> 
+	  	
+		<meta charset="UTF-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0,maximum-scale=1">
+		
+		<title>Online invoice and accounting- MyExps.com</title>
+		<meta name="description" content="Create your free account with MyExps. Hassle-free online invoice and accounts software. Raise your invoice with a payment link and get paid in a limited time.">
+
+
+		<!-- Loading third party fonts -->
+		<link href="http://fonts.googleapis.com/css?family=Roboto+Condensed:300,400,700|" rel="stylesheet" type="text/css">
+		<link href="fonts/font-awesome.min.css" rel="stylesheet" type="text/css">
+		<link href="fonts/lineo-icon/style.css" rel="stylesheet" type="text/css">
+
+		<!-- Loading main css file -->
+		<link rel="stylesheet" href="style.css">
+		
+		<!--[if lt IE 9]>
+		<script src="js/ie-support/html5.js"></script>
+		<script src="js/ie-support/respond.js"></script>
+		<![endif]-->
+		<style type="text/css">
+			
+			.btn
+                  {
+                    padding:5px;
+                    background:#3097d1;
+                    border:1px solid #0c4565;
+                    border-radius:3px;
+                    color: #fff;
+                  }
+
+		</style>
+		<script src="//code.tidio.co/cjcpou54uhsjmqa6ovyhqxxcosqlpx8e.js" async></script>
+	</head>
+
+
+	<body>
+		
+		<div id="site-content">
+			<header class="site-header">
+				<div class="top-header" style="padding: 0px">
+					<div class="container">
+						<a href="index.php" id="branding">
+							<img src="images/logo.png" alt="Company Name" class="logo">
+							<!--<div class="logo-text">
+								<h1 class="site-title">MyExps</h1>
+								<small class="description">my exps manager</small>
+							</div>-->
+						</a> <!-- #branding -->
+					
+						<div class="right-section pull-right" style="padding-top: 40px;">
+							<a href="#" class="phone" style="font-size: 18px;padding-right: 25px"> <img src="images/icon-phone.png" class="icon" alt="accounting solution"> +971 58 606 5564 ( Extn 61)</a>
+							<a href="register.php" class="button">Signup</a>
+							<a href="accounts/index.php" class="button" >Login</a>
+							<!--<form action="#" class="search-form">
+								<input type="text" placeholder="Search...">
+								<button type="submit"><img src="images/icon-search.png" alt=""></button>
+							</form>-->
+						</div>
+					</div> <!-- .container -->
+				</div> <!-- .top-header -->
+
+				
+				<div class="bottom-header">
+					<div class="container">
+						<div class="main-navigation">
+							<button type="button" class="menu-toggle"><i class="fa fa-bars"></i></button>
+							<ul class="menu">
+								<li class="menu-item"><a href="index.php" class="active">Home</a></li>
+								<li class="menu-item"><a href="about.php">About</a></li>
+								<li class="menu-item"><a href="features.php">Features</a></li>
+								<li class="menu-item"><a href="pricing.php">Pricing</a></li>
+								<li class="menu-item"><a href="support.php">Support</a></li>
+								<li class="menu-item"><a href="https://myexps.com/blog/">Blog</a></li>
+								<li class="menu-item"><a href="contact.php">Contact</a></li>
+							</ul> <!-- .menu -->
+						</div> <!-- .main-navigation -->
+						
+						<div class="social-links">
+							<a href="https://www.facebook.com/MyExpsuae"><i class="fa fa-facebook"></i></a>
+							<a href="https://twitter.com/ExpsMy"><i class="fa fa-twitter"></i></a>							
+							<a href="https://www.instagram.com/my_exps/"><i class="fa fa-instagram"></i></a>
+							<a href="https://www.linkedin.com/company/69181602/admin/"><i class="fa fa-linkedin-square"></i></a>
+							<a href="https://www.pinterest.com/MyExps/_created/"><i class="fa fa-pinterest"></i></a>
+						</div>
+						
+						<div class="mobile-navigation"></div>
+					</div>
+				</div>
+			</header> <!-- .site-header -->
+			<form method="post" action="" id="register" name="register">
+			<main class="main-content">
+				<div class="fullwidth-block greet-section">
+					<div class="container">
+						<!--<div class="row">
+							<div class="col-md-12">
+								<div class="feature">
+									<h1>About MyExps</h1>
+									
+								</div>
+							</div>
+						</div>--> <!-- .row -->
+						
+						<div class="row"> 
+							<div class="feature">
+								<div class="col-md-4">
+									<img src="images/about.png" alt="MyExps" valign="top" align="left" /></div>
+								<div class="col-md-8" align="left">
+									<p>Managing Invoices has been always an issue in past.</p>
+
+<p>ERP software is boring, expensive, hard to setup and frustrating to use. Ample hours are spend in configuring the product to your need. Asking for professional help costs a lot. In the end, the solution becomes so complex that businesses need to hire full-time employees to babysit the software sit with every department in the company to ensure all needs to meet with one software.</p>
+
+<p>At wethinksolution, we believe every business deserves simple software - software thatís ready to go, easy to setup and use, and requires minimal customization. All of our products live up to this promise and are backed by our world-class support. And the best part is, we give you trial that free and there after its minimal cost to manage you bills slowly progressing to finance.</p>
+
+<p>Today, over 40 companies trust our software to run their business. Here is the beginning join us to let us help you to run your business efficiently.</p>
+								</div>
+							</div>
+							</div>
+
+					</div> <!-- .container -->
+				</div> <!-- .fullwidth-block -->
+			</main>
+			</form>
+			<div class="site-footer">
+				<div class="widget-area">
+					<div class="container">
+						<div class="row">
+							<div class="col-xs-12 col-sm-4 col-md-2">
+								<div class="widget">
+									<ul class="no-bullet">
+										<li><a href="reviews.php">Reviews</a></li>
+										<li><a href="terms.php">Terms of Use</a></li>
+										<li><a href="privacy.php">Privacy Policy</a></li>										
+									</ul>
+								</div>
+							</div>
+							<div class="col-xs-12 col-sm-4 col-md-2"><h2>265+ <br/>Happy Users</h2></div>
+							<div class="col-xs-12 col-sm-4 col-md-2"><h2>5991+ <br/>Total Invoices</h2></div>
+							<div class="col-xs-12 col-sm-4 col-md-2"></div>
+							<div class="col-xs-12 col-sm-4 col-md-2">
+								<div class="widget">
+									<a href="index.php" id="branding">
+									<img src="images/logo.png" alt="Company Name" class="logo">
+									<!--<div class="logo-text">
+										<h1 class="site-title">MyExps</h1>
+										<small class="description">my exps manager</small>
+									</div>-->
+								</a> <!-- #branding -->
+								</div>
+							</div>
+							<div class="col-xs-12 col-sm-4 col-md-2">
+								<div class="widget">
+									<address>MyExps is one of the easiest way to create, send Invoice and get paid for invoice by your customers.</address><br/>
+									<a href="#">+971 58 606 5564 ( 61)</a><br/>
+									<a href="mailto:info@MyExps.com">info@myexps.com</a>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+
+				<div class="bottom-footer">
+					<div class="container">
+						<nav class="footer-navigation">
+							<a href="index.php">Home</a>
+							<a href="about.php">About</a>
+							<a href="features.php">Features</a>
+							<a href="pricing.php">Pricing</a>
+							<a href="support.php">Support</a>
+							<a href="contact.php">Contact</a>
+						</nav>
+
+						<div class="colophon">© 2020 Designed with love by WethinkSolution. All rights reserved.</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<script src="js/jquery-1.11.1.min.js"></script>
+		<script src="http://maps.google.com/maps/api/js?sensor=false&amp;language=en"></script>
+		<script src="js/plugins.js"></script>
+		<script src="js/app.js"></script>
+		<script type="text/javascript">
+			
+			$(document).ready(function(){
+
+   $("#email").keyup(function(){
+
+      var email = $(this).val().trim();
+
+      if(email != ''){
+
+         $.ajax({
+            url: 'check_user.php',
+            type: 'post',
+            data: {username: email},
+            success: function(response){
+        if(response==1)
+        {
+                $('#uname_response').html("<img src='images/tick.png'>"+"<span style='color: green;'> Good to go!</span>");
+        $('#uname_checker').val("1");
+        }
+        else
+        {
+        $('#uname_response').html("<img src='images/error.png'>"+"<span style='color: red;'> Email already Registered</span>");
+        $('#uname_checker').val("0");
+        }
+
+             }
+         });
+      }else{
+         $("#uname_response").html("");
+     $('#uname_checker').val("0");
+      }
+
+    });
+
+ });
+ function clearUname()
+ {
+  var checker = $('#uname_checker').val();
+  
+  if(checker=="" || checker==0)
+  {
+    $("#uname_response").html("");
+    $('#email').val("");
+  }
+ }
+
+		</script>
+		
+	</body>
+
+</html>
